2d4266319d2d783d7f709e8b7432957782ad91db
Jeremy Walton <jthelegoman@gmail.com>
2014-01-24 11:00:54 -0500

    Added discard pile class, player class and finished deck and playing card(for now)

---
+++ CrazyEightsModel/JPWDiscardPile.m
@@ -0,0 +1,13 @@
+//
+//  JPWDiscardPile.m
+//  CrazyEightsModel
+//
+//  Created by Jeremy on 1/24/14.
+//  Copyright (c) 2014 Jeremy-Walton. All rights reserved.
+//
+
+#import "JPWDiscardPile.h"
+
+@implementation JPWDiscardPile
+
+@end
---
+++ CrazyEightsModel/JPWPlayer.h
@@ -0,0 +1,13 @@
+//
+//  JPWPlayer.h
+//  CrazyEightsModel
+//
+//  Created by Jeremy on 1/24/14.
+//  Copyright (c) 2014 Jeremy-Walton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JPWPlayer : NSObject
+
+@end
